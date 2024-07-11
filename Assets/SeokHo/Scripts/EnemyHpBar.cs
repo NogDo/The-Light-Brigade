3ea419bb906496c7f50f19af8451394ede9182bf
유석호 <dbtjrgh0522@naver.com>
2024-07-11 21:33:53 +0900

    07.11 일반 적 HP바 구현 및 플레이어를 향해 타겟 고정 구현

--- Assets/SeokHo/Scripts/EnemyHpBar.cs
+++ Assets/SeokHo/Scripts/EnemyHpBar.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class EnemyHpBar : MonoBehaviour
 {
+    private Canvas canvas;
+    private RectTransform rectParent; // 부모의 rectTransform 변수를 저장할 변수
+    private RectTransform rectHp; // 자신의 rectTransform 저장할 변수
+
+    // HideInInspector는 해당 변수 숨기기, 굳이 보여줄 필요가 없을 때 
+    public Vector3 offset = Vector3.zero; // HpBar 위치 조절용, offset은 어디에 HpBar를 위치 출력할지
+    public Transform enemyTr; // 적 캐릭터의 위치
+
+    void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        rectParent = canvas.GetComponent<RectTransform>();
+        rectHp = this.gameObject.GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        rectParent.transform.position = enemyTr.position + offset;
+
+    }
+
 
 }
