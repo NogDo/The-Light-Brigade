c28390e36ff4d0ba04d7ecc593f5e20f0fbf43c1
유석호 <dbtjrgh0522@naver.com>
2024-07-17 21:35:55 +0900

    07.13 적 상태패턴 구현중

---
+++ Assets/SeokHo/Scripts/CChasePlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CChasePlayer : MonoBehaviour
+{
+    CNormalEnemy normalenemy;
+
+    private void Start()
+    {
+        gameObject.GetComponent<Collider>().enabled = true;
+        normalenemy = transform.parent.GetComponent<CNormalEnemy>();
+    }
+
+    // 탐지 거리 10f
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        else if(other.CompareTag("Player"))
+        {
+            Debug.Log("닿았음");
+            // 공격 상태로 전환
+            normalenemy.ChangeState(State.CHASE);
+            normalenemy.target = other.transform;
+        }
+
+    }
+}
